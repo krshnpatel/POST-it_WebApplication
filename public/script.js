36175a147f0ac61ec8a73ab8972be27c243d9164
Kripal Patel <kpate222@uwo.ca>
2016-11-07 18:08:40 +0000

    Adding files for lab 3 part 2 (FINAL)

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+// This function executes the HTTP GET request
 function executeGET(call)
 {
     $.ajax({
@@ -5,114 +6,81 @@ function executeGET(call)
         type: 'GET',
         complete: function(result) {
             var data = JSON.parse(result.responseText);
-            console.log("GET");
-            console.log(data.length);
             
+            // If this function is called the first time then only display messages
             if (call == "first")
             {
-                displayMessages(data, "append");
+                displayMessages(data);
             }
-            else
+            else // Else, delete all elements on the screen and display them again
             {
                 deleteAllElements();
-                displayMessages(data, "append");
+                displayMessages(data);
             }
             
-            //executeDELETEall(data);
-            setTimeout(executeGET, 1000);
+            //executeDELETEall(data);  //<-- DO NOT UNCOMMENT THIS LINE, it deletes all the entries from the database
+            
+            setTimeout(executeGET, 1000); // This calls the executeGET function every second
         }
     });
 }
 
+// This function executes the HTTP POST request
 function executePOST(userText, afterDelete)
 {
     $.ajax({
         url : "https://se3316a-lab3-kpate222.c9users.io/api/msgs",
         type: "POST",
-        data: {key:userText.substring(1)},
+        data: {key:userText.substring(1)}, // Stores the text after the hashtag as the key
         complete: function(result)
         {
-            var data = JSON.parse(result.responseText);
-            
-            console.log("POST");
-            console.log(data);
+            var data = JSON.parse(result.responseText); // Parses data into a JSON array
             
             deleteAllElements();
-            displayMessages(data, "append");
+            displayMessages(data);
         },
     });
 }
 
+// This function executes the HTTP DELETE request on a specific message
 function executeDELETE(msgID)
 {
-    console.log(msgID);
     var URL = 'https://se3316a-lab3-kpate222.c9users.io/api/msgs/' + msgID;
     
     $.ajax({
         url: URL.toString(),
         type: 'DELETE',
-        complete: function(result) {
-            console.log("DELETE");
-            console.log(JSON.parse(result.responseText));
-        }
     });
 }
 
-function getMsgId(key)
+// This function creates a DOM element for each message and adds it to the HTML document
+function displayMessages(msgs)
 {
-    $.ajax({
-        url: 'https://se3316a-lab3-kpate222.c9users.io/api/msgs',
-        type: 'GET',
-        complete: function(result) {
-            
-            var data = JSON.parse(result.responseText);
-            
-            for (var i = 0; i < data.length; i++)
-            {
-                if (data[i].key == key)
-                {
-                    console.log("get id - " + data[i]._id);
-                    executeDELETE(data[i]._id);
-                }
-            }
-        }
-    });
-}
-
-function displayMessages(msgs, sortMethod)
-{
-    var endpoint = 0;
+    var endpoint = 0; // Endpoint is used so only the last 20 elements are displayed
     if (msgs.length > 20)
     {
-        endpoint = msgs.length - 20;
+        endpoint = msgs.length - 20; // Calculates the endpoint
     }
-    
-    console.log(endpoint);
     
     for (var i = msgs.length - 1; i >= endpoint; i--)
     {
-        if (!document.body.contains(document.getElementById(msgs[i].key)))
+        if (!document.body.contains(document.getElementById(msgs[i].key))) // Checks if the specified element exists
         {
+            // Creates a new div element
             var newMsg = document.createElement('div');
             
+            // Adds an id, classes, and text content to the newly created element
             newMsg.id = msgs[i].key;
             newMsg.className = 'text-center submittedText';
             newMsg.textContent = "#" + msgs[i].key;
             
-            console.log("adding element");
-            
-            if (sortMethod == "append")
-            {
-                document.getElementById('messages').appendChild(newMsg);
-            }
-            else if (sortMethod == "insert")
-            {
-                document.getElementById('messages').insertBefore(newMsg, document.getElementById('messages').childNodes.item(0));
-            }
+            // Appends the newly created element to the children of #messages element
+            document.getElementById('messages').appendChild(newMsg);
         }
     }
 }
 
+// This function deletes all the children of the #messages element
 function deleteAllElements()
 {
     var messages = document.getElementById('messages');
@@ -123,12 +91,37 @@ function deleteAllElements()
     }
 }
 
-function verifyMessage(userText) {
+// This function GETs the _id of the specified message and DELETEs it
+function deleteMsgId(key)
+{
+    $.ajax({
+        url: 'https://se3316a-lab3-kpate222.c9users.io/api/msgs',
+        type: 'GET',
+        complete: function(result) {
+            var data = JSON.parse(result.responseText);
+            
+            // Find the message _id using its key
+            for (var i = 0; i < data.length; i++)
+            {
+                if (data[i].key == key)
+                {
+                    executeDELETE(data[i]._id); // Delete the message with its _id
+                    break;
+                }
+            }
+        }
+    });
+}
+
+// This function verifies the user entered text
+function verifyMessage(userText)
+{
+    // Check if the user text starts with a # and has a length no more than 200 characters
     if (userText.charAt(0) == '#' && (userText.length > 1 && userText.length <= 200))
     {
         if (document.body.contains(document.getElementById(userText.substring(1))))
         {
-            getMsgId(userText.substring(1));
+            deleteMsgId(userText.substring(1));
             return true;
         }
         else
@@ -147,6 +140,7 @@ $(document).ready(function() {
     
     executeGET("first");
     
+    // If the submit button is clicked then verify the message and POST it
     $('#submitButton').click(function(e) {
         var userText = $('#userText').val();
         
@@ -158,6 +152,7 @@ $(document).ready(function() {
         $('#userText').val("");
     });
     
+    // If the enter key is pressed in the text box then verify the message and POST it
     $('#userText').bind("enterKey",function(e) {
         
         var userText = $('#userText').val();
@@ -178,16 +173,17 @@ $(document).ready(function() {
     });
 });
 
-function executeDELETEall(msgs) {
-    
+
+// ===================================================================================
+// This function is not used unless you want to delete all entries from the database
+// This function can be used by uncommenting the line in executeGET function
+function executeDELETEall(msgs)
+{
     for (var i = 0; i < msgs.length; i++)
     {
         $.ajax({
             url: 'https://se3316a-lab3-kpate222.c9users.io/api/msgs/' + msgs[i]._id,
             type: 'DELETE',
-            success: function(result) {
-                console.log("deleted");
-            }
         });
     }
 }
